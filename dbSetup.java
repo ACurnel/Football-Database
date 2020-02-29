af70cc3bab61afdae90ee1005a5932be9003c188
alexchristian09 <alexchristian09@tamu.edu>
2020-02-28 19:32:19 -0600

    Working Entity & Column Retrieval
    Column Retrieval currently just prints the list of columns

--- dbSetup.java
+++ dbSetup.java
@@ -4,7 +4,7 @@ CSCE 315
 Database setup example
 Purpose: Keeping usernames and password private.
 */
-public final class dbSetup  {
-  public static final String user = "skyelar";
-  public static final String pswd = "427000724";
-}//end class
+public final class dbSetup {
+	public static final String user = "alexchristian09";
+	public static final String pswd = "926006726";
+}// end class
--- jdbcpostgreSQLGUI.java
+++ jdbcpostgreSQLGUI.java
@@ -1,5 +1,6 @@
 import java.sql.*;
 import javax.swing.JOptionPane;
+import java.util.ArrayList;
 //import java.sql.DriverManager;
 /*
 Robert lightfoot
@@ -7,62 +8,76 @@ CSCE 315
 9-25-2019
  */
 public class jdbcpostgreSQLGUI {
-  public static void main(String args[]) {
-    dbSetup my = new dbSetup();
-    //Building the connection
-     Connection conn = null;
-     try {
-        Class.forName("org.postgresql.Driver");
-        conn = DriverManager.getConnection("jdbc:postgresql://csce-315-db.engr.tamu.edu/team_nand",
-           my.user, my.pswd);
-     } catch (Exception e) {
-        e.printStackTrace();
-        System.err.println(e.getClass().getName()+": "+e.getMessage());
-        System.exit(0);
-     }//end try catch
-     JOptionPane.showMessageDialog(null,"Opened database successfully");
-     String cus_lname = "";
-     try{
-     //create a statement object
-       Statement stmt = conn.createStatement();
-       //create an SQL statement
-       String[] choices = { "team_code", "offensive_records", "C", "D", "E", "F" };
-       String input = (String) JOptionPane.showInputDialog(null, "Choose now...",
-        "The Choice of a Lifetime", JOptionPane.QUESTION_MESSAGE, null, // Use
-                                                                        // default
-                                                                        // icon
-        choices, // Array of choices
-        choices[1]); // Initial choice
-       System.out.println(input);
+	public static void main(String args[]) {
+		dbSetup my = new dbSetup();
+		// Building the connection
+		Connection conn = null;
+		try {
+			Class.forName("org.postgresql.Driver");
+			conn = DriverManager.getConnection("jdbc:postgresql://csce-315-db.engr.tamu.edu/team_nand", my.user,
+					my.pswd);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		} // end try catch
+		JOptionPane.showMessageDialog(null, "Opened database successfully");
+		String output = "";
+		try {
+			// create a statement object
+			Statement stmt = conn.createStatement();
+			// create an SQL statement
+			// ENTITIES
+			String[] choices = { "Players", "Teams", "Seasons", "Player_Records", "Games", "Stadiums", "Positions", "Offensive_Records", "Defensive_Records", "Conferences", "Statistics", "Drives" };
+			String input = (String) JOptionPane.showInputDialog(null, "Choose now...", "What are you interested in looking up?",
+					JOptionPane.QUESTION_MESSAGE, null, // Use
+														// default
+														// icon
+					choices, // Array of choices
+					choices[0]); // Initial choice
+			System.out.println(input);
+			String sqlStatement = "";
+			// REQUEST COLUMN LIST
+			sqlStatement = "Select * from " + input + " limit 0";
+			ResultSet columns = stmt.executeQuery(sqlStatement);
+			ResultSetMetaData columnstuff = columns.getMetaData();
+			int colcnt = columnstuff.getColumnCount();
+			ArrayList<String> columnlist = new ArrayList<String>();
+			for (int i = 1; i < colcnt; i++) {
+				columnlist.add(columnstuff.getColumnName(i));
+			}
+			JOptionPane.showMessageDialog(null, "Available Columns:\n");
+			for (int j = 0; j < columnlist.size(); j++) {
+			//System.out.println(columnlist.get(j) + "\n");
+				output += columnlist.get(j) + "\n";
+			}		
+			/*if (input == "offensive_records")
+				sqlStatement = "SELECT player_code FROM offensive_records limit 10";
+			else if (input == "team_code")
+				sqlStatement = "SELECT name FROM teams limit 10";
+			// send statement to DBMS
+			ResultSet result = stmt.executeQuery(sqlStatement);
 
-       String sqlStatement = "";
-       if(input == "offensive_records")
-          sqlStatement = "SELECT player_code FROM offensive_records limit 10";
-       else if(input == "team_code")
-          sqlStatement = "SELECT name FROM teams limit 10";
-       //send statement to DBMS
-       ResultSet result = stmt.executeQuery(sqlStatement);
-
-       //OUTPUT
-       JOptionPane.showMessageDialog(null,"Player code from offensive_records");
-       //System.out.println("______________________________________");
-       while (result.next()) {
-         //System.out.println(result.getString("cus_lname"));
-        if(input == "offensive_records")
-         cus_lname += result.getString("player_code")+"\n";
-        else if(input == "team_code")
-          cus_lname += result.getString("name")+"\n";
-       }
-   } catch (Exception e){
-     JOptionPane.showMessageDialog(null,"Error accessing Database.");
-   }
-   JOptionPane.showMessageDialog(null,cus_lname);
-    //closing the connection
-    try {
-      conn.close();
-      JOptionPane.showMessageDialog(null,"Connection Closed.");
-    } catch(Exception e) {
-      JOptionPane.showMessageDialog(null,"Connection NOT Closed.");
-    }//end try catch
-  }//end main
-}//end Class
+			// OUTPUT
+			JOptionPane.showMessageDialog(null, "Player code from offensive_records");
+			// System.out.println("______________________________________");
+			while (result.next()) {
+				// System.out.println(result.getString("cus_lname"));
+				if (input == "offensive_records")
+					output += result.getString("player_code") + "\n";
+				else if (input == "team_code")
+					output += result.getString("name") + "\n";
+			}*/
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Error accessing Database.");
+		}
+		JOptionPane.showMessageDialog(null, output);
+		// closing the connection
+		try {
+			conn.close();
+			JOptionPane.showMessageDialog(null, "Connection Closed.");
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Connection NOT Closed.");
+		} // end try catch
+	}// end main
+}// end Class
